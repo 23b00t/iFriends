c79570b22168235e3e0698c404e1b2e98c2f99eb
Michael Nelin <mishanelin17@gmail.com>
2023-02-04 16:22:20 +0100

    sdas

--- app/models/imaginary_friend.rb
+++ app/models/imaginary_friend.rb
@@ -1,5 +1,6 @@
 class ImaginaryFriend < ApplicationRecord
   belongs_to :user
+  has_many :bookings
   validates :name, presence: true
   validates :name, uniqueness: { scope: :user_id }
   validates :price, presence: true
