a67ff6fac84ebe81c6b7cf6ee8dd51884eec37a6
Michael Nelin <mishanelin17@gmail.com>
2023-02-04 16:42:24 +0100

    add private methods

--- app/controllers/imaginary_friends_controller.rb
+++ app/controllers/imaginary_friends_controller.rb
@@ -1,14 +1,12 @@
 class ImaginaryFriendsController < ApplicationController
+  before_action :set_ifriend
+
   def index
     @imaginary_friends = ImaginaryFriend.all
   end
 
   def new
     @imaginary_friend = ImaginaryFriend.new()
-  end
-
-  def show
-    @imaginary_friend = ImaginaryFriend.find(params[:id])
   end
 
   def create
@@ -18,10 +16,6 @@ class ImaginaryFriendsController < ApplicationController
     else
       render :new, status: :unprocessable_entity
     end
-  end
-
-  def edit
-    @imaginary_friend = ImaginaryFriend.find(params[:id])
   end
 
   def update
@@ -34,13 +28,17 @@ class ImaginaryFriendsController < ApplicationController
   end
 
   def destroy
-    @imaginary_friend = ImaginaryFriend.find(params[:id])
     @imaginary_friend.destroy
-
     redirect_to imaginary_friends_path, status: :see_other
   end
 
-  def imfriend_params
+  private
+
+  def set_ifriend
+    @booking = ImaginaryFriend.find(params[:id])
+  end
+
+  def ifriend_params
     params.require(:imaginary_friend).permit(:name, :description, :price, :special_abilities, :rented)
   end
 end
