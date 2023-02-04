79cc1270eda57e040c999bd998d5c7afa66f7ba5
amelia kroner <amelia.rkroner@gmail.com>
2023-02-04 14:47:24 +0100

    resources for routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+
+  resources :imaginary_friends do
+    resources :bookings
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
