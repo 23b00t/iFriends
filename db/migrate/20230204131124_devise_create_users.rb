5030eb3216bd6bda9ce9d8743623a1b092ce56e0
Daniel Kipp <daniel.kipp@gmail.com>
2023-02-04 15:50:01 +0100

    signup, in and out

--- db/migrate/20230204131124_devise_create_users.rb
+++ db/migrate/20230204131124_devise_create_users.rb
@@ -6,8 +6,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-      t.string :first_name, null: false
-      t.string :last_name, null: false
+      t.string :first_name, null: false, default: ""
+      t.string :last_name, null: false, default: ""
 
       ## Recoverable
       t.string   :reset_password_token
