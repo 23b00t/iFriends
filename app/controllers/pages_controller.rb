61c72402e83dfe8686705f0a663f9450bea81de2
Michael Nelin <mishanelin17@gmail.com>
2023-02-07 20:28:58 +0100

    better homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,11 @@
 class PagesController < ApplicationController
+  before_action :authenticate_user!
+
   def home
+    @imaginary_friends = ImaginaryFriend.all.select { |m| m.user_id == current_user.id }
+  end
+
+  def my_rents
+    @rent_friends = Booking.all.select { |m| m.user_id == current_user.id }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
+  get "/my rents", to: "pages#my_rents"
+
   resources :imaginary_friends do
     resources :bookings, only: %i[create index]
   end
