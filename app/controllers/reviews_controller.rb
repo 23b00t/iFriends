b536ad078d876e6acb5c6f59e4720de6b7aadfa8
amelia kroner <amelia.rkroner@gmail.com>
2023-02-11 13:02:32 +0100

    all basic review functionality done

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,9 @@
 class ReviewsController < ApplicationController
   before_action :set_ifriend
+  before_action :set_review, only: %i[destroy]
 
   def create
+    # raise
     @review = Review.new(review_params)
     @review.user = current_user
     @review.imaginary_friend = @imaginary_friend
@@ -12,9 +14,7 @@ class ReviewsController < ApplicationController
     end
   end
 
-  def delete
-    # raise
-    @review = Review.find(params[:id])
+  def destroy
     @review.destroy
     redirect_to imaginary_friend_path(@imaginary_friend)
   end
@@ -23,6 +23,10 @@ class ReviewsController < ApplicationController
 
   def review_params
     params.require(:review).permit(:comment, :rating)
+  end
+
+  def set_review
+    @review = Review.find(params[:id])
   end
 
   def set_ifriend
