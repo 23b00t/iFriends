f0df977ee899b31c798a0f4ee86d2c2b61763ed0
amelia kroner <amelia.rkroner@gmail.com>
2023-02-14 21:31:26 +0100

    front-end changes

--- app/models/imaginary_friend.rb
+++ app/models/imaginary_friend.rb
@@ -9,8 +9,4 @@ class ImaginaryFriend < ApplicationRecord
   validates :name, presence: true
   validates :name, uniqueness: { scope: :user_id }
   validates :price, presence: true
-  # validates :special_abilities, inclusion: { in: ["Flying", "Warm hugs", "Invisibility", "Visibility",
-                                                  # "Cooking", "Great advice"],
-                                                  # message: "%<category> is not a valid category.
-                                                  # Flying, Warm hugs, Invisibility, Visibility, Cooking, Great advice" }
 end
